13bd910d15c0e4cca9b96ca975fe8b3e8bc22cdf
Wellington Araújo <wellingtonaraujo777@gmail.com>
2023-01-18 15:47:04 -0300

    Adicionando simples TextView

--- app/src/main/java/com/wells/agenda/MainActivity.java
+++ app/src/main/java/com/wells/agenda/MainActivity.java
@@ -2,6 +2,7 @@ package com.wells.agenda;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
@@ -12,5 +13,8 @@ public class MainActivity extends Activity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Toast.makeText(this, "Well Araujo", Toast.LENGTH_LONG).show();
+        TextView aluno = new TextView(this);
+        aluno.setText("Well Araujo");
+        setContentView(aluno);
     }
 }
