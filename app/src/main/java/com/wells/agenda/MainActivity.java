8fdb76b7e8fa18586787807954172a30e811f3e3
Wellington Araújo <wellingtonaraujo777@gmail.com>
2023-01-18 15:15:32 -0300

    Adicionando comportamentos na Activity

--- app/src/main/java/com/wells/agenda/MainActivity.java
+++ app/src/main/java/com/wells/agenda/MainActivity.java
@@ -1,6 +1,16 @@
 package com.wells.agenda;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
 
 public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Toast.makeText(this, "Well Araujo", Toast.LENGTH_LONG).show();
+    }
 }
