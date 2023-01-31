98483fa2969bf4cfcbb5a147b3b558582754e37a
Wellington Araújo <wellingtonaraujo777@gmail.com>
2023-01-31 15:44:54 -0300

    Ajustes no layout e remoção de código morto na MainActivity

--- app/src/main/java/com/wells/agenda/MainActivity.java
+++ app/src/main/java/com/wells/agenda/MainActivity.java
@@ -1,14 +1,9 @@
 package com.wells.agenda;
 
 import android.app.Activity;
-import android.database.DataSetObserver;
 import android.os.Bundle;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.ListAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
@@ -25,7 +20,7 @@ public class MainActivity extends Activity {
         Toast.makeText(this, "Well Araujo", Toast.LENGTH_LONG).show();
         setContentView(R.layout.activity_main);
         List<String> alunos = new ArrayList<String>(Arrays.asList("Alex", "Fran", "Jose", "Maria", "Ana"));
-        ListView listaDeAlunos = findViewById(R.id.activity_main_lisa_de_alunos);
+        ListView listaDeAlunos = findViewById(R.id.activity_main_lista_de_alunos);
         listaDeAlunos.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, alunos));
 
     }
