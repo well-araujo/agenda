c12184151723791745692a34b324c16657bcf0fc
Wellington Araújo <wellingtonaraujo777@gmail.com>
2023-02-01 15:02:01 -0300

    Adição do título Lista de Alunos e ícone de adição de alunos

--- app/src/main/java/com/wells/agenda/MainActivity.java
+++ app/src/main/java/com/wells/agenda/MainActivity.java
@@ -7,18 +7,20 @@ import android.widget.ListView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Toast.makeText(this, "Well Araujo", Toast.LENGTH_LONG).show();
         setContentView(R.layout.activity_main);
+        setTitle("Lista de Alunos");
         List<String> alunos = new ArrayList<String>(Arrays.asList("Alex", "Fran", "Jose", "Maria", "Ana"));
         ListView listaDeAlunos = findViewById(R.id.activity_main_lista_de_alunos);
         listaDeAlunos.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, alunos));
